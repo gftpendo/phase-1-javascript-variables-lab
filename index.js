2185eac33a493efdf544a8c0c87dfcfadd0cbb80
Gift Pendo <gyftpendoh@gmail.com>
2024-12-08 09:47:13 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution in this file!
+
+const companyName = 'Scuber'; // 1. Defined as a const and set to 'Scuber'
+
+let mostProfitableNeighborhood = 'Chelsea'; // 2. Defined using let and set to 'Chelsea'
+
+let companyCeo = 'Susan Smith'; // 3. Defined using let and set to 'Susan Smith'
